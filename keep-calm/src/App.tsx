6d8b79315d879a1e061bb01bf12a208e9c206ca6
IlyaLagoonov <iliay338@gmail.com>
2023-02-06 08:25:03 +0300

    fix pages

---
+++ keep-calm/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./Components";
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+      Привет, Андрей
+    </div>
+  );
+}
+
+export default App;
